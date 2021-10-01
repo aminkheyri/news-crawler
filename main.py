6b0c85bded201386c4084ce87aeb65e1c0ce37ba
amin <kheyrriamin@yahoo.com>
2021-10-01 04:39:41 +0330

    add informations in a dictationary

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import xmltodict
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
 
-list_of_times = []
+
+dict_of_news = {}
+list_of_news = []
+list_of_requested_times = []
 primary_url = 'https://www.varzesh3.com/'
 url = "https://www.varzesh3.com/sitemap/news"
 result = requests.get(url)
@@ -15,59 +18,55 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 
-start_date = date(2021, 9, 13)
-end_date = date(2021, 9, 14)
+start_date = date(2021, 9, 21)
+end_date = date(2021, 9, 22)
 for single_date in daterange(start_date, end_date):
-    list_of_times.append(single_date.strftime("%Y-%m-%d"))
-print(list_of_times)
+    list_of_requested_times.append(single_date.strftime("%Y-%m-%d"))
 
 
-def varzesh3_crawler():
-    list_of_urls = []
-    list_of_news = []
-    for i in list_of_times:
+def get_content_of_varzesh3_url(given_url):
+    raw_url = requests.get(url=given_url)
+    content = BeautifulSoup(raw_url.text, 'html.parser')
+    final_content = content.find('div', {'class': 'col-xs-12 news-page--news-text text-justify'})
+    if final_content == None:
+        pass
+    else:
+        return final_content.text
+
+
+def get_content_of_khabarvarzeshi_url(given_url):
+    raw_url = requests.get(url=given_url)
+    content = BeautifulSoup(raw_url.text, 'html.parser')
+    content = content.find('div', {'class': 'item-text'})
+    return content.text
+
+
+def get_varzesh3_information():
+    for i in list_of_requested_times:
         for j in range(len(data['urlset']['url'])):
             if i == data['urlset']['url'][j]['lastmod'][0:10]:
-                list_of_urls.append(primary_url + data['urlset']['url'][j]['loc'][:14])
+                dict_of_news['url'] = primary_url + data['urlset']['url'][j]['loc'][:14]
+                dict_of_news['title'] = data['urlset']['url'][j]['loc'][14:]
+                dict_of_news['lastmod'] = data['urlset']['url'][j]['lastmod']
+                dict_of_news['content'] = get_content_of_varzesh3_url(primary_url + data['urlset']['url'][j]['loc'][:14])
+                list_of_news.append(dict_of_news.copy())
 
-    for url in list_of_urls:
-        raw_url = requests.get(url=url)
-        content = BeautifulSoup(raw_url.text, 'html.parser')
-        final_content = content.find('div', {'class': 'col-xs-12 news-page--news-text text-justify'})
-        if final_content == None:
-            continue
-        else:
-            list_of_news.append(final_content.text)
-        for news in list_of_news:
-            with open('news.txt', 'w+', encoding="utf8") as f:
-                f.write(news)
+    return list_of_news
 
 
-def khabar_varzeshi_crawler():
-    list_of_urls = []
-    list_of_news = []
-    for single_date in daterange(start_date, end_date):
-        list_of_times.append(single_date.strftime("%Y-%m-%d"))
-
-    for i in list_of_times:
+def get_khabarvarzeshi_information():
+    for i in list_of_requested_times:
         for j in range(len(data['urlset']['url'])):
             if i == data['urlset']['url'][j]['lastmod'][0:10]:
-                list_of_urls.append(data['urlset']['url'][j]['loc'][:43])
-
-    for url in list_of_urls:
-        raw_url = requests.get(url=url)
-        content = BeautifulSoup(raw_url.text, 'html.parser')
-        a = content.find('a', {'itemdrop': 'headline'})
-        b = content.find('div', {'class': 'item-text'})
-        list_of_news.append(a)
-        list_of_news.append(b)
-
-    for news in list_of_news:
-        with open('news.txt', 'w+', encoding="utf8") as f:
-            f.write(news)
+                dict_of_news['url'] = data['urlset']['url'][j]['loc'][:43]
+                dict_of_news['title'] = data['urlset']['url'][j]['loc'][43:]
+                dict_of_news['lastmod'] = data['urlset']['url'][j]['lastmod']
+                dict_of_news['content'] = get_content_of_khabarvarzeshi_url(data['urlset']['url'][j]['loc'][:43])
+                list_of_news.append(dict_of_news.copy())
+    return list_of_news
 
 
 if url == "https://www.varzesh3.com/sitemap/news":
-    print(varzesh3_crawler())
+    print(get_varzesh3_information())
 elif url == "https://www.khabarvarzeshi.com/sitemap/news/sitemap.xml":
-    print(varzesh3_crawler())
+    print(get_khabarvarzeshi_information())
