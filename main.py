0ff22d147f59221d9c5d3c5bf0896ace34f2f715
amin <kheyrriamin@yahoo.com>
2021-10-03 10:59:59 +0330

    Turning lastmod to datetime object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import xmltodict
-from datetime import date, timedelta
+import datetime
 from bs4 import BeautifulSoup
 
 
@@ -15,11 +15,11 @@ data = xmltodict.parse(result.content)
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+        yield start_date + datetime.timedelta(n)
 
 
-start_date = date(2021, 9, 21)
-end_date = date(2021, 9, 22)
+start_date = datetime.date(2021, 9, 21)
+end_date = datetime.date(2021, 9, 22)
 for single_date in daterange(start_date, end_date):
     list_of_requested_times.append(single_date.strftime("%Y-%m-%d"))
 
@@ -47,7 +47,8 @@ def get_varzesh3_information():
             if i == data['urlset']['url'][j]['lastmod'][0:10]:
                 dict_of_news['url'] = primary_url + data['urlset']['url'][j]['loc'][:14]
                 dict_of_news['title'] = data['urlset']['url'][j]['loc'][14:]
-                dict_of_news['lastmod'] = data['urlset']['url'][j]['lastmod']
+                dict_of_news['lastmod'] = datetime.datetime.strptime(
+                    data['urlset']['url'][j]['lastmod'], '%y-%m-%d')
                 dict_of_news['content'] = get_content_of_varzesh3_url(primary_url + data['urlset']['url'][j]['loc'][:14])
                 list_of_news.append(dict_of_news.copy())
 
@@ -60,7 +61,8 @@ def get_khabarvarzeshi_information():
             if i == data['urlset']['url'][j]['lastmod'][0:10]:
                 dict_of_news['url'] = data['urlset']['url'][j]['loc'][:43]
                 dict_of_news['title'] = data['urlset']['url'][j]['loc'][43:]
-                dict_of_news['lastmod'] = data['urlset']['url'][j]['lastmod']
+                dict_of_news['lastmod'] = datetime.datetime.strptime(data['urlset']['url'][j]['lastmod'],
+                                                                     '%y-%m-%d')
                 dict_of_news['content'] = get_content_of_khabarvarzeshi_url(data['urlset']['url'][j]['loc'][:43])
                 list_of_news.append(dict_of_news.copy())
     return list_of_news
